c5b7e4b1ba8e8bb8e9a0d3db8581f8eef9618926
Mikhail Latypov <hodkonem1984@gmail.com>
2024-04-07 00:50:22 +0300

    Create methods add(), size(), and get()

--- src/main/java/ru/latypov/my_list/my_list_array/MyDynamicArray.java
+++ src/main/java/ru/latypov/my_list/my_list_array/MyDynamicArray.java
@@ -6,20 +6,33 @@ class MyDynamicArray<E> implements ResizableList<E> {
 
     private E[] values;
 
-    public MyDynamicArray(E[] values) {
-        values = (E[]) new Object();
+    public MyDynamicArray() {
+        values = (E[]) new Object[0];
     }
 
-    public MyDynamicArray(ResizableList<? extends E> collection)
+    public MyDynamicArray(ResizableList<? extends E> collection) {
+        for (E element : collection) {
+            add(element);
+        }
+    }
 
     @Override
     public boolean add(E e) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            values[values.length - 1] = e;
+            return true;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return values.length;
     }
 
     @Override
@@ -30,5 +43,9 @@ class MyDynamicArray<E> implements ResizableList<E> {
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    public E get(int index) {
+        return values[index];
     }
 }
--- src/test/java/ru/latypov/my_list/my_list_array/MyDynamicArrayTest.java
+++ src/test/java/ru/latypov/my_list/my_list_array/MyDynamicArrayTest.java
@@ -2,10 +2,42 @@ package ru.latypov.my_list.my_list_array;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 class MyDynamicArrayTest {
 
     @Test
     void testAddMethodShouldIncreaseSizeByOne() {
-        MyDynamicArray<Integer> myDynamicArray;
+        MyDynamicArray<Integer> myDynamicArray = new MyDynamicArray<>();
+        int initialSize = myDynamicArray.size();
+        myDynamicArray.add(6);
+        int newSize = myDynamicArray.size();
+        assertEquals(initialSize + 1, newSize);
     }
+
+    @Test
+    void testAddMethodShouldAddElementToEndOfArray() {
+        MyDynamicArray<Integer> myDynamicArray = new MyDynamicArray<>();
+        myDynamicArray.add(3);
+        myDynamicArray.add(10);
+        assertEquals(Integer.valueOf(3), myDynamicArray.get(0));
+    }
+
+    @Test
+    void testAddMethodShouldNotModifyArrayIfExceptionIsThrown() {
+        MyDynamicArray<Integer> myDynamicArray = new MyDynamicArray<>();
+        myDynamicArray.add(5);
+        assertEquals(1, myDynamicArray.size());
+
+        try {
+            myDynamicArray.add(10);
+            fail("Expected ClassException to be thrown");
+        } catch (ClassCastException e) {
+            assertEquals(1, myDynamicArray.size());
+        }
+    }
+
+
+
 }
